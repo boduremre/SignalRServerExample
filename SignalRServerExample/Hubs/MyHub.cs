296125db96a55680357eefc01c23a2e1cdd95405
Emre Bodur <emrebodurbto@gmail.com>
2023-04-28 13:57:16 +0300

    Proje dosyası ekle.

---
+++ SignalRServerExample/Hubs/MyHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerExample.Hubs
+{
+    public class MyHub : Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                await Clients.All.SendAsync("ReceiveMessage", string.Format("Gönderdiğiniz Mesaj: {0}",message));
+            }
+            else
+            {
+                await Clients.All.SendAsync("ReceiveMessage", "Lütfen mesaj giriniz.");
+            }
+        }
+    }
+}
